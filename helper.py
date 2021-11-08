d739dc6e09faa27deec5cd83ac59fd6e9f15c0d7
Adam Džadoň <adam.dzadon.gmail.com>
2021-11-08 17:20:29 +0100

    Create helping script

---
+++ helper.py
@@ -0,0 +1,50 @@
+from typing import List, Tuple, Callable
+
+def hex2dec(hex_num: str) -> int:
+	"""
+	Function resposible for converting hexadecimal number to decimal number
+
+	hex_num - string with hexadecimal number
+	"""
+
+	hex_num = hex_num.replace("\n","")
+	convertion = {
+	  "a": 10, "b": 11, "c": 12, 
+	  "d": 13, "e": 14, "f": 15
+	  }
+	decimal = 0
+
+	for index in range(len(hex_num)):
+
+		if hex_num[index].lower() in convertion:
+			decimal += (16**(len(hex_num) - index - 1)) * convertion[hex_num[index].lower()]
+		else:
+			decimal += (16**(len(hex_num) - index - 1)) * int(hex_num[index].lower())
+
+	return decimal
+
+
+def hexColor(color: str) -> Tuple:
+	"""
+	Function responsible for converting color hexcode to rgb tuple
+
+	color - string with color hexcode
+	"""
+	color = color.replace("\n", "")
+	r = hex2dec(color[1:3])
+	g = hex2dec(color[3:5])
+	b = hex2dec(color[5:])
+
+	return (r, g, b)
+
+def serve_pil_image(img) -> Callable:
+	"""
+	Allows to save PIL image object to a
+	virtual file in memory and then return
+	it as a HTTP response
+	"""
+
+	img_io = BytesIO()
+	img.save(img_io, 'PNG', quality=70)
+	img_io.seek(0)
+	return send_file(img_io, mimetype='image/png')
--- main.py
+++ main.py
@@ -5,60 +5,12 @@ import graph_back.my_plot_lib as my_plot_lib
 import relations_back.out as o
 from PIL import Image
 from typing import Tuple, Callable
+import helper
 
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.static_folder = r'public\static'
-
-def hex2dec(hex_num: str) -> int:
-	"""
-	Function resposible for converting hexadecimal number to decimal number
-
-	hex_num - string with hexadecimal number
-	"""
-
-	hex_num = hex_num.replace("\n","")
-	convertion = {
-	  "a": 10, "b": 11, "c": 12, 
-	  "d": 13, "e": 14, "f": 15
-	  }
-	decimal = 0
-
-	for index in range(len(hex_num)):
-
-		if hex_num[index].lower() in convertion:
-			decimal += (16**(len(hex_num) - index - 1)) * convertion[hex_num[index].lower()]
-		else:
-			decimal += (16**(len(hex_num) - index - 1)) * int(hex_num[index].lower())
-
-	return decimal
-
-
-def hexColor(color: str) -> Tuple:
-	"""
-	Function responsible for converting color hexcode to rgb tuple
-
-	color - string with color hexcode
-	"""
-	color = color.replace("\n", "")
-	r = hex2dec(color[1:3])
-	g = hex2dec(color[3:5])
-	b = hex2dec(color[5:])
-
-	return (r, g, b)
-
-def serve_pil_image(img) -> Callable:
-	"""
-	Allows to save PIL image object to a
-	virtual file in memory and then return
-	it as a HTTP response
-	"""
-
-	img_io = BytesIO()
-	img.save(img_io, 'PNG', quality=70)
-	img_io.seek(0)
-	return send_file(img_io, mimetype='image/png')
 
 
 
@@ -114,7 +66,7 @@ def get_graph():
 	selected = request.form.get("selected")
 	color = request.form.get("color")
 
-	r, g, b = hexColor(color)
+	r, g, b = helper.hexColor(color)
 
 	if selected == "linear":
 		my_plot_lib.draw_plot(canvas, my_plot_lib.linear, (r, g, b))
@@ -123,7 +75,7 @@ def get_graph():
 	elif selected == "cubic":
 		my_plot_lib.draw_plot(canvas, my_plot_lib.cubic, (r, g, b))
 
-	return serve_pil_image(canvas)
+	return helper.serve_pil_image(canvas)
 
 
 @app.route('/solve_relation', methods=["get", 'post'])
